e45ee6241014951ff0d9e783b42c5abc69dc9ede
CarlosRoqueT <91689269+CarlosRoqueT@users.noreply.github.com>
2022-12-05 00:36:06 -0600

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,75 @@
+[
+    {
+        "foto": "https://i.ibb.co/xStNXHD/carlos-v-f.png",
+        "nombre": "Carlos V",
+        "marca": "Nestle",
+        "descripcion": "Chocolate de crema marca Nestle",
+        "precio": "$35 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/QMxZLSV/pulparindo-f.png",
+        "nombre": "Pulparindo",
+        "marca": "de la Rosa",
+        "descripcion": "Barras de dulce con chile marca de la Rosa",
+        "precio": "$28 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/bgt5Gj6/pica-fresa-f.png",
+        "nombre": "Pica Fresa",
+        "marca": "Vero",
+        "descripcion": "Bolitas agridulces de fresa marca Vero",
+        "precio": "$27 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/R6fCntm/jumbo-cereza-f.png",
+        "nombre": "Jumbo Cereza",
+        "marca": "de la Rosa",
+        "descripcion": "Paletas de caramelo sabor cereza marca de la Rosa",
+        "precio": "$45 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/vDkVG5Y/pi-a-loca-f.png",
+        "nombre": "Piña Loca",
+        "marca": "Alteño",
+        "descripcion": "Paletas de caramelo sabor piña con chile marca Alteño",
+        "precio": "$38 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/4PXdCTd/rockaleta-f.png",
+        "nombre": "Rockalesta",
+        "marca": "Sonrics",
+        "descripcion": "Paleta con capas de caramelo suave con chile y relleno de chicle",
+        "precio": "$35 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/1vQ7cBq/rebanaditas-f.png",
+        "nombre": "Rebanaditas",
+        "marca": "CP Products",
+        "descripcion": "Paletas de caramelo sabor sandia con cubierta de chile",
+        "precio": "$35 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/5vbLJ1n/betamex-f.png",
+        "nombre": "Betamex",
+        "marca": "Betamex",
+        "descripcion": " Varitas de chamoy espolvoreadas con chile del que no pica",
+        "precio": "$60 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/6bTKjPF/pica-pepino-f.png",
+        "nombre": "Pica Pepino",
+        "marca": "Alteño",
+        "descripcion": "Paletas de caramelo sabor pepino con chile ",
+        "precio": "$25 c/u"
+    },
+    {
+        "foto": "https://i.ibb.co/K00ZX3h/aciduladito-f.png",
+        "nombre": "Aciduladito",
+        "marca": "de la Rosa",
+        "descripcion": "Pequeños caramelos acidos y picositos con sabores variados ",
+        "precio": "$26 c/u"
+    }
+
+
+
+]
